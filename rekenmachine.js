fe450461f67aabcf5dcc475c637431108457622b
Chantal Rikse <c.rikse@outlook.com>
2021-10-11 20:09:29 +0200

    third commit

--- rekenmachine.js
+++ rekenmachine.js
@@ -1,6 +1,41 @@
 function plus() {
     var a = Number.parseInt(document.getElementById('invoer1').value);
     var b = Number.parseInt(document.getElementById('invoer2').value);
-    var resultaat = a + b;
+    var c = check();
+    if (check() == true) {
+        var resultaat = a + b;
+        document.getElementById("resultaat").innerText = resultaat;
+    } else{
+        document.getElementById("resultaat").innerText = "Vink de checkbox aan wanneer je wilt optellen.";
+    }
+    return c;
+}
+
+function min() {
+    var a = Number.parseInt(document.getElementById('invoer1').value);
+    var b = Number.parseInt(document.getElementById('invoer2').value);
+    var resultaat = a - b;
     document.getElementById("resultaat").innerText = resultaat;
+}
+
+function delen() {
+    var a = Number.parseInt(document.getElementById('invoer1').value);
+    var b = Number.parseInt(document.getElementById('invoer2').value);
+    if (b == 0) {
+        document.getElementById("resultaat").innerText = "Kan niet delen door 0.";
+    } else {
+        var resultaat = a / b;
+        document.getElementById("resultaat").innerText = resultaat;
+    }
+}
+
+function keer() {
+    var a = Number.parseInt(document.getElementById('invoer1').value);
+    var b = Number.parseInt(document.getElementById('invoer2').value);
+    var resultaat = a * b;
+    document.getElementById("resultaat").innerText = resultaat;
+}
+
+function check() {
+    return document.getElementById("check").checked;
 }
